a4d08be2994000dd9aeacb3bf7858977210519cc
mkovarky <mikhail.kovarsky@gmail.com>
2020-04-28 18:27:11 +0300

    add PostmanEchoTest

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalToObject;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostToPostman() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalToObject("some data"))
+        ;
+    }
+}
